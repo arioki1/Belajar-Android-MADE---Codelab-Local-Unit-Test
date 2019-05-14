ddfcff384e5a65a038b326f36d9af07a3ec56897
Yoga Setiawan <mryoga1995@gmail.com>
2019-05-15 04:19:06 +0700

    Unit Test

---
+++ app/src/test/java/com/example/myunittesting/MainPresenterTest.java
@@ -0,0 +1,39 @@
+package com.example.myunittesting;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+
+@RunWith(MockitoJUnitRunner.class)
+
+public class MainPresenterTest {
+
+    @Mock
+    private MainPresenter presenter;
+    private MainView view;
+
+    @Test
+    public void volume() {
+    }
+
+    @Test
+    public void calculateVolume() {
+    }
+
+    @Before
+    public void setUp() {
+        view = mock(MainView.class);
+        presenter = new MainPresenter(view);
+    }
+
+    @Test
+    public void testVolumeWithIntegerInput() {
+        double volume = presenter.volume(2, 8, 1);
+        assertEquals(16, volume, 0.0001);
+    }
+}
