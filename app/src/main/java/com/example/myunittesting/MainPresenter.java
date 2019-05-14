ad08d15be2cf289d86b60fd4af30b323166fd3ba
Yoga Setiawan <mryoga1995@gmail.com>
2019-05-15 04:19:42 +0700

    Unit Test Fix

--- app/src/main/java/com/example/myunittesting/MainPresenter.java
+++ app/src/main/java/com/example/myunittesting/MainPresenter.java
@@ -8,7 +8,7 @@ public class MainPresenter {
     }
 
     public double volume(double length, double width, double height) {
-        return length / width * height;
+        return length * width * height;
     }
 
     public void calculateVolume(double lenght, double width, double height) {
